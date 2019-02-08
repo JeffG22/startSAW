f9d2ddb0d41d6f5e1a9e49c777e4aacaeaa0a515
frcassi <federico.cassissa@outlook.com>
2019-02-08 17:01:31 +0100

    Added database interaction when inserting a new proposal. Dummy coordinates and id. No session control.

--- connection.php
+++ connection.php
@@ -1,11 +1,6 @@
 <?php
-    $host = "localhost";
-    $user = "S4328810";
-    $pwd = "TiganiForever";
-    $dbname = "S4328810";
-
     function dbConnect() {
-        $con = mysqli_connect($host, $user, $pwd, $dbname);
+        $con = mysqli_connect("localhost", "S4328810", "TiganiForever", "S4328810");
         if (mysqli_connect_errno($con)) {
             $_SESSION['message'] = "Error";
             return false;
--- public_html/php/receive_new_proposal.php
+++ public_html/php/receive_new_proposal.php
@@ -1,10 +1,8 @@
 <?php
+    include("../../connection.php");
     session_start();
     function uploadPicture() {
-        if(!isset($_FILES['picture']) || !is_uploaded_file($_FILES['picture']['tmp_name'])) {
-            echo "Errore nel caricamento dell'immagine. L'immagine potrebbe non esistere o essere troppo grande (max 4 MB).";
-        } else {
-            //$uploaddir = './uploads/';  
+        if(isset($_FILES['picture']) && is_uploaded_file($_FILES['picture']['tmp_name'])) {
             $uploaddir = "../userpics/";
             $filename = (microtime(true)*10000);
             $uploadfile = $uploaddir.$filename.".".pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);
@@ -26,7 +24,7 @@
     }      
 
     if (empty($_POST)) {
-        $_SESSION['message'] = "Errore nella ricezione dei dati. L'errore potrebbe essere dovuto a un'immagine troppo grande (max 4MB)";
+        $_SESSION['message'] = "Nessun dato ricevuto.";
         header("location: new_proposal.php");
         exit();
     }
@@ -35,15 +33,41 @@
         $_SESSION['message'] = "Errore. Compila tutti i campi richiesti.";
         header("location: new_proposal.php");
         exit();
+    } else {
+        $name = htmlspecialchars($_POST['name']);
+        $description = htmlspecialchars($_POST['description']);
+        $available_pos = intval($_POST['available_positions']);
+        if ($available_pos <= 0) {
+            $_SESSION['message'] = "Controlla il numero di posizioni disponibili. Deve essere un numero positivo.";
+            header("location: new_proposal.php");
+            exit();
+        }
     }
 
     $file = uploadPicture();
 
-    if (!$file) {
+    $con = dbConnect();
+
+    if (!$con) {
         header("location: new_proposal.php");
         exit();
     }
 
+    $stmt = mysqli_prepare($con, "INSERT INTO proposal (name, description, picture, address, coord_x, coord_y, available_positions, date_inserted, proposer_id) values (?,?,?,?,?,?,?,?,?)");
+
+    if (empty($_POST['address'])) {
+        $address = NULL;
+    } else {
+        $address = htmlspecialchars($_POST['address']);
+    }
+
+    $date = date("Y-m-d");
+    $coord_x = 123.45;
+    $coord_y = -566.1221;
+    $id = 112;
+    mysqli_stmt_bind_param($stmt, "ssssddisi", $name, $description, $file, $address, $coord_x, $coord_y, $available_pos, $date, $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
     
 ?>
 

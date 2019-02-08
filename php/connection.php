83ea736363b1384108c097884353ff9992306f17
frcassi <federico.cassissa@outlook.com>
2019-02-08 14:28:20 +0100

    Implemented function to connect to a database

--- php/connection.php
+++ php/connection.php
@@ -3,3 +3,14 @@
     $user = "S4328810";
     $pwd = "TiganiForever";
     $dbname = "S4328810";
+
+    function dbConnect() {
+        $con = mysqli_connect($host, $user, $pwd, $dbname);
+        if (mysqli_connect_errno($con)) {
+            $_SESSION['message'] = "Error";
+            return false;
+        } else {
+            return $con;
+        }
+    }
+    

512204281cca4871821fb9c19d0fa069e3ffe63f
frcassi <federico.cassissa@outlook.com>
2019-02-11 07:54:58 +0100

    Restored connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+    function dbConnect() {
+        $con = mysqli_connect("localhost", "S4328810", "TiganiForever", "S4328810");
+        if (mysqli_connect_errno($con)) {
+            return false;
+        } else {
+            return $con;
+        }
+    }
+?>
+    

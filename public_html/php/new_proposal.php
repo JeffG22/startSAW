b5d767f3ad2e0fb421dcc03ce319ff064b30aef9
frcassi <federico.cassissa@outlook.com>
2019-02-13 15:47:55 +0100

    Separated check function into external file

--- public_html/php/new_proposal.php
+++ public_html/php/new_proposal.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="../js/inputChecks.js"></script>
 </head>
 <body>
     <a href="index_proposals.php">^ Home</a>
@@ -41,29 +42,5 @@
         <br>
         <input type="submit">
     </form>
-
-    
-    <script>
-        function checkPicture() {
-            /** If supported by browser, checks file type and size.
-                These checks are repeated server-side. */
-            if (window.FileReader) 
-            {
-                fileSize = document.getElementById("upload_picture").files[0].size;
-                fileType = document.getElementById("upload_picture").files[0].type;
-                try {
-                    if (!["image/png", "image/jpeg", "image/jpg", "image/bmp"].includes(fileType)) {
-                        throw "Attenzione: formato file non supportato. Puoi caricare immagini in formato JPEG, PNG e BMP.";
-                    } else if (fileSize > 4194304) {// Max size = 4MB
-                        throw "Attenzione: il file caricato è troppo grosso. La dimensione massima consentita è 4MB.";
-                    }
-                } 
-                catch (err) {
-                    alert(err);
-                    document.getElementById("upload_picture").value = null;
-                }
-            }
-        }
-    </script>
 </body>
 </html>

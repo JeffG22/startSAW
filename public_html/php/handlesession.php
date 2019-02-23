6d9c1caef3b1b42bdde4f2fbf1459cd532feb704
JeffG22 <j.giliberti22@gmail.com>
2019-02-23 19:07:35 +0100

    changes on registration, login and profile edit

--- public_html/php/handlesession.php
+++ public_html/php/handlesession.php
@@ -53,7 +53,6 @@
     // My session regenerate id function
     function my_session_regenerate_id() {
         safe_session_start();
-        isDeletedOrInactive(); // If this session has already been renewed
         // -- renewing -- using a timestamp to avoid error with multiple requests (e.g. Ajax)
         $_SESSION['deleted_time'] = time() + 120; // Set deleted timestamp. Session data must not be deleted immediately.
         session_regenerate_id(); // Create new session without destroying the old one

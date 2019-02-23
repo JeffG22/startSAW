3c32486458d74bc96d70bef08bdf4a76d8a0b527
Travali <sarahtravali@gmail.com>
2019-02-23 17:22:33 +0100

    Incomplete edits. I have to decide what to do...

--- public_html/user.php
+++ public_html/user.php
@@ -9,17 +9,17 @@
 <head>
   <title>Profilo</title>
     
-  <!--Bootstrap-->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
-  			integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-					integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+  		  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+			integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
-					integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+			integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
-					integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+			integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" 
-				integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+		  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<!--Bootstrap-->
 
   <meta charset="utf-8">
@@ -30,7 +30,7 @@
   <script src="js/jquery-3.1.0.min.js"></script>
   <script src="js/jquery.easing.min.js"></script>
 	
-  <!--link rel="stylesheet" href="css/global.css"-->
+  <link rel="stylesheet" href="css/global.css">
   <link rel="stylesheet" href="css/user.css">
 </head>
 
@@ -54,11 +54,14 @@
                     <!-- SIDEBAR USER TITLE -->
                     <div class="profile-usertitle">
                         <div class="profile-usertitle-name">
-                            Placeholder
+                            <?php
+                                $userid = $_SESSION['user_id'];
+                                echo $userid;
+                            ?>
                         </div>
-                        <div class="profile-usertitle-job">
-                            Placeholder
-                        </div>
+                        <!--div class="profile-usertitle-job">
+                            Volontario o organizzazione
+                        </div-->
                     </div>
                     <!-- END SIDEBAR USER TITLE -->
 
@@ -82,7 +85,7 @@
                             </li>
                             <li>
                                 <a href="#" target="_blank">
-                                <i class="fas fa-list-alt"></i>
+                                <i class="fas fa-heart"></i>
                                 Proposte Accettate </a>
                             </li>
                         </ul>

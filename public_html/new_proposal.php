97e273face6525ee5ec4c044b23b4d221e0c67d3
frcassi <federico.cassissa@outlook.com>
2019-02-23 12:46:48 +0100

    Consolidated new proposal into a single page

--- public_html/new_proposal.php
+++ public_html/new_proposal.php
@@ -3,9 +3,11 @@
     include_once("php/utilities.php");
     include_once("php/handlesession.php");
     include_once("../connection.php");
-    //my_session_start();
-    debug_session_start();
-    //my_session_is_valid(); // If not logged in, redirect to home page
+    
+    my_session_start();
+    // If a user is not logged in and lands on this page, redirect to login
+    if (!my_session_is_valid())
+        header("Location: login.php");
 
     try {
         if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
@@ -15,11 +17,12 @@
             $available_pos = "available_positions";
             $upload_picture = "upload_picture";
             $address = "address";
+
         // Checks on user input
             if (empty($_POST[$name]) || !checksOnProposalName($_POST[$name]))
                 throw new InvalidArgumentException($name);
             if (empty($_POST[$description]) || !checksOnDescription($_POST[$description]))
-                throw new InvalidArgumentException($descriptio);
+                throw new InvalidArgumentException($description);
             if (empty($_POST[$available_pos]) || !checksOnAvailablePos($_POST[$available_pos]))
                 throw new InvalidArgumentException($available_pos);
             if (!empty($_POST[$upload_picture]) && !($file = uploadPicture()))
@@ -31,13 +34,52 @@
             $description_value = nl2br(sanitize_inputString($_POST[$description]));
             $available_pos_value = intval($_POST[$available_pos]);
 
-            if (!($conn = get_dbconnection()))
-                throw new Exception("sql ".mysqli_connect_error());
+            if (empty($_POST[$address])) {
+                $address_value = NULL; 
+                $lat = NULL;
+                $long = NULL;
+            } else {    // Convert adress to coordinates using OpenStreetMap geocoding API
+                $address_value = sanitize_inputString($_POST[$address]);
+                $request = "http://nominatim.openstreetmap.org/search.php?q=".urlencode($address_value)."&email=ktmdy@hi2.in&format=json";
+                $response = file_get_contents($request);
+                $location = json_decode($response, true); // true = return as associative array
+                if ($location != NULL && !empty($location)) {
+                    $lat = $location[0]['lat']."\n";
+                    $lon = $location[0]['lon'];
+                }
+            }
 
-            $stmt = mysqli_prepare($con, "INSERT INTO proposal 
-                (name, description, picture, address, lat, lon, 
-                    available_positions, date_inserted, proposer_id) 
-                VALUES (?,?,?,?,?,?,?,?,?)");
+            $date = date("Y-m-d");  // Returns current date formatted as YYYY-MM-DD
+
+            $user_id = $_SESSION['userId'];
+
+            if (!($conn = dbConnect()))
+                throw new Exception("sql ".mysqli_connect_error());
+            
+            $query = "INSERT INTO proposal 
+                    (name, description, picture, address, lat, lon, 
+                        available_positions, date_inserted, proposer_id) 
+                    VALUES (?,?,?,?,?,?,?,?,?)";
+
+            if (!($stmt = mysqli_prepare($conn, $query)))
+                throw new Exception("mysqli prepare".mysqli_error($conn));
+
+            if (!mysqli_stmt_bind_param($stmt, "ssssddisi", $name_value, $description_value, $file, $address_value, $lat, $lon, 
+                                            $available_pos_value, $date, $user_id))
+                throw new Exception("mysqli bind param");
+            
+            if (!mysqli_stmt_execute($stmt))
+                throw new InvalidArgumentException("mysqli execute".$stmt->error);
+
+            if(mysqli_affected_rows($conn) == 1) {
+                $_SESSION['message'] = "Inserimento completato correttamente.";
+            } else {
+                $_SESSION['message'] = "Errore nell'inserimento. Riprova.";
+            }
+            
+            mysqli_stmt_close($stmt);
+    
+            mysqli_close($conn);
         }
     } catch (Exception $ex) {
         $error_flag = true;
@@ -71,6 +113,20 @@
     <script src="js/include.js"></script> 
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
+
+
+    <script>
+        "use strict"; //necessario per strict mode
+        // creare un'array associativo "messaggio -> errore"           
+        var err_array = {
+                'name' : 'Nome non valido.',
+                'description' : 'Descrizione non valida.',
+                'available_pos' : 'Il numero di posizioni disponibili deve essere compreso tra 1 e 300.',
+                'upload_picture' : 'Errore nel caricamento dell\'immagine. Assicurati che il tipo di file sia supportato (JPG, PNG, BMP) e che le dimensioni non superino i 4MB',
+                'address' : 'Indirizzo non valido.',
+                'altro' : 'Inserimento non riuscito, si prega di riprovare'
+        };
+    </script>
 </head>
 <body>
 
@@ -82,7 +138,7 @@
     <a href="php/index_proposals.php">^ Home</a>
     <?php
         if (isset($_SESSION['message'])) {
-            echo "<div color=\"white\">".$_SESSION['message']."</div>";
+            echo "<div>".$_SESSION['message']."</div>";
             unset($_SESSION['message']);
         }
     ?>  

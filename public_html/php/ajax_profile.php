b5ba2464847d7a5fa942704b09109481cab4a741
Travali <sarahtravali@gmail.com>
2019-02-26 14:14:11 +0100

    Working ajax profiles (with all fields from user table)

--- public_html/php/ajax_profile.php
+++ public_html/php/ajax_profile.php
@@ -11,21 +11,20 @@
     $res = mysqli_query($conn, $query1);
     $rev = mysqli_fetch_array($res, MYSQLI_NUM);
     $new_id = $rev[0];
-    if($rev[1] == 'person') {
-        $re = mysqli_query($conn, "SELECT user.display_name, 
-                                          user.type, person.picture,
-                                          person.description
-                                   FROM user, person
-                                   WHERE person.id = user.user_id
-                                   AND person.id =".$new_id);
-    } else {
-        $re = mysqli_query($conn, "SELECT user.display_name, 
+    //if($rev[1] == 'person') {
+        $re = mysqli_query($conn, "SELECT display_name, 
+                                          type, picture,
+                                          description
+                                   FROM user
+                                   WHERE user_id =".$new_id);
+    /*} else {
+        $re = mysqli_query($conn, "SELECT display_name, 
                                           user.type, organization.picture,
                                           organization.description
                                    FROM user, organization
                                    WHERE organization.id = user.user_id,
                                    AND organization.id =".$new_id);
-    }
+    }*/
     $rex = mysqli_fetch_array($re, MYSQLI_NUM);
     if(mysqli_num_rows($re) == 1)
         echo json_encode($rex);

70c2cbaf05cd769d3e0ce94297ecc70b18968388
frcassi <federico.cassissa@outlook.com>
2019-02-26 16:07:51 +0100

    Removed useless functions

--- public_html/php/utilities.php
+++ public_html/php/utilities.php
@@ -1,81 +1,8 @@
 <?php
-    function getUserName($con, $id) {
-        $result = mysqli_query($con, "SELECT name, surname
-                                      FROM user, person
-                                      WHERE user_id = ".$id." AND user_id = person.id");
-        if (!$result) {
-            return FALSE;
-        } else if (mysqli_num_rows($result) != 0) {
-            $row = mysqli_fetch_assoc($result);
-            return $row['name']." ".$row['surname'];
-        } else {    // If no result, it might be an organization
-            $result = mysqli_query($con, "SELECT name
-                                      FROM user, organization
-                                      WHERE user_id = ".$id." AND user_id = organization.id");
-            if (!$result) {
-                return FALSE;
-            } else if (mysqli_num_rows($result) != 0) {
-                $row = mysqli_fetch_assoc($result);
-                return $row['name'];
-            } else {
-                return FALSE;
-            }
-        }
-    }
-
-    function getUserDesc($con, $id) {
-        $result = mysqli_query($con, "SELECT description
-                                      FROM user, person
-                                      WHERE user_id = ".$id." AND user_id = person.id");
-        if (!$result) {
-            return FALSE;
-        } else if (mysqli_num_rows($result) != 0) {
-            $row = mysqli_fetch_assoc($result);
-            return $row['description'];
-        } else {    // If no result, it might be an organization
-            $result = mysqli_query($con, "SELECT description
-                                      FROM user, organization
-                                      WHERE user_id = ".$id." AND user_id = organization.id");
-            if (!$result) {
-                return FALSE;
-            } else if (mysqli_num_rows($result) != 0) {
-                $row = mysqli_fetch_assoc($result);
-                return $row['description'];
-            } else {
-                return FALSE;
-            }
-        }
-    }
-
-
-    function getUserRole($con, $id) {
-        $result = mysqli_query($con, "SELECT type
-                                      FROM user
-                                      WHERE user_id = ".$id);
-        if (!$result) {
-            return FALSE;
-        } else if (mysqli_num_rows($result) != 0) {
-            $row = mysqli_fetch_assoc($result);
-            return $row['type'];
-        } 
-    }
-
-
     // Simple function to stop current script and navigate to a specified location
     function navigateTo($location) {
         header("Location: ".$location);
         exit();
-    }
-
-    // Checks if given user_id is a person.
-    function isPerson($con, $user_id) {
-        $res = mysqli_query($con, "SELECT user_id
-                        FROM user
-                        WHERE user_id = ".$user_id." AND user_id IN (SELECT id FROM person)");
-        if(mysqli_num_rows($res) == 1)
-            return TRUE;
-        else 
-            return FALSE;
     }
 
     // Prints informations about a specified proposal, passed as an argument in form of a complete
